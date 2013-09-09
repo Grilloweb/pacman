0de07a236d8c30a48745c4f468051e9783fa176e
grillo-ogilvy <leonardo.teixeira@oirecife.com.br>
2013-09-09 14:44:07 -0300

    mecanica de furar a parede e aparecer no outro lado

--- public/javascripts/pacman.js
+++ public/javascripts/pacman.js
@@ -8,11 +8,10 @@ Pac =
 	ctx 			: canvas.getContext('2d'),
 	tamanhoBoneco 	: 10,
 	tamanhoPasso 	: 5,
-	velocidadeBoneco: 300,
+	velocidadeBoneco: 100,
 	alturaCenario 	: $(canvas).height(),
 	larguraCenario	: $(canvas).width(),
-	posicaoBoneco1	: {s:'b',x:0,y:0},
-	posicaoBoneco2	: {s:'b',x:$(canvas).width()-10,y:0},
+	posicaoBoneco	: {s:'b',x:0,y:0},
 
 	//inicializador do projeto
 	init : function()
@@ -23,17 +22,90 @@ Pac =
 	//metodo que desenha o cenario
 	movimentaBoneco : function()
 	{
+		var seta = Pac.posicaoBoneco.s;
+
+		$(document).on('keydown', function(e)
+		{
+			seta = Pac.setas(e.keyCode);
+
+			if(seta == false)
+				seta = Pac.posicaoBoneco.s;
+		});
+
 		setInterval(function()
 		{
 			Pac.limpaTela();
 
-			if(Pac.posicaoBoneco1.s == 'b')
-				Pac.posicaoBoneco1 = {s:'b', x:Pac.posicaoBoneco1.x, y:Pac.posicaoBoneco1.y+Pac.tamanhoBoneco};
+			switch (seta || Pac.posicaoBoneco.s)
+			{
+				case 'e' :
+					var x = Pac.posicaoBoneco.x - Pac.tamanhoBoneco;
+					if(x < 0)
+						x = Pac.larguraCenario - Pac.tamanhoBoneco;
+					else
+						x = Pac.posicaoBoneco.x - Pac.tamanhoBoneco;
 
-			Pac.boneco(Pac.posicaoBoneco1.x,Pac.posicaoBoneco1.y);
+					Pac.posicaoBoneco = {s:seta, x:x, y:Pac.posicaoBoneco.y};
+					break;
+				case 'c' :
+					var y = Pac.posicaoBoneco.y - Pac.tamanhoBoneco;
+					if(y < 0)
+						y = Pac.alturaCenario - Pac.tamanhoBoneco;
+					else
+						y = Pac.posicaoBoneco.y - Pac.tamanhoBoneco;
 
+					Pac.posicaoBoneco = {s:seta, x:Pac.posicaoBoneco.x, y:y};
+					break;
+				case 'd' :
+					var x = Pac.posicaoBoneco.x + Pac.tamanhoBoneco;
+					if(x >= Pac.larguraCenario)
+						x = 0 + Pac.tamanhoBoneco;
+					else
+						x = Pac.posicaoBoneco.x + Pac.tamanhoBoneco;
+
+					Pac.posicaoBoneco = {s:seta, x:x, y:Pac.posicaoBoneco.y};
+					break;
+				case 'b' :
+					var y = Pac.posicaoBoneco.y + Pac.tamanhoBoneco;
+					if(y >= Pac.alturaCenario)
+						y = 0 + Pac.tamanhoBoneco;
+					else
+						y = Pac.posicaoBoneco.y + Pac.tamanhoBoneco;
+
+					Pac.posicaoBoneco = {s:seta, x:Pac.posicaoBoneco.x, y:y};
+					break;
+			}
+			
+			console.log(Pac.posicaoBoneco, seta);		
+
+			Pac.boneco(Pac.posicaoBoneco.x,Pac.posicaoBoneco.y);
+		
 		}, Pac.velocidadeBoneco);
+	},
 
+	setas : function(keycode)
+	{
+		var seta = false;
+
+		switch (keycode)
+		{
+			case 37 :
+				seta = 'e';
+				break;
+			case 38 :
+				seta = 'c';
+				break;
+			case 39 :
+				seta = 'd';
+				break;
+			case 40 :
+				seta = 'b';
+				break;
+			default : 
+				seta = false;
+		}
+
+		return seta;
 	},
 
 	//metodo com o desenho de 1 box do cenario
